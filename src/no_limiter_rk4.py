12beb8669e13d2b040fce2a560beb7ef8e6f5eb1
Jonathan Palafoutas <jpalafou@princeton.edu>
2023-01-11 14:45:08 -0500

    combine 1D advection solution files

--- src/no_limiter_rk4.py
+++ src/no_limiter_rk4.py
@@ -4,13 +4,15 @@ import math
 import matplotlib.pyplot as plt
 from util.solve import AdvectionSolver
 
-a = 1  # choose a positive value of the 'wind' direction
 
-# domain
-x_bounds = [0, 1]
-h = 0.02
-T = 2
-Dt = 0.8 * h / a
+# inputs
+ic_type = "square"  # initial condition type
+orders = range(1, 6)  # solve using schemes of these orders
+a = 1  # tranpsort speed
+x_bounds = [0, 1]  # spatial domain
+h = 0.02  # grid size
+T = 2  # solving time
+Dt = 0.8 * h / a  # time step size
 
 # array of x-values
 x_interface = np.arange(x_bounds[0], x_bounds[1] + h, h)
@@ -20,12 +22,17 @@ x = 0.5 * (x_interface[:-1] + x_interface[1:])  # x at cell centers
 t = np.arange(0, T + Dt, Dt)
 
 # initial values of u
-u0 = np.cos(2 * math.pi * x)
+if ic_type == "sinus":
+    u0 = np.cos(2 * math.pi * x)
+elif ic_type == "square":
+    u0 = np.array(
+        [np.heaviside(i - 0.25, 1) - np.heaviside(i - 0.75, 1) for i in x]
+    )
 
 # advect for T orbits and the initial state should match the final state
 # do this for many orders
 plt.plot(x, u0, label="t = 0")
-for order in range(1, 8):
+for order in orders:
     advection_solution = AdvectionSolver(x0=u0, t=t, h=h, a=a, order=order)
     advection_solution.rk4()
     u = advection_solution.x
